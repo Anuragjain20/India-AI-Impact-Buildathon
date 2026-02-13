a5add2fa4f7ef5964705877a6844c57351faf3b6
Anupam Pandey <88734093+anupampadey71@users.noreply.github.com>
2026-02-13 22:10:24 +0530

    Add files via upload

---
+++ final-Honeypot detection/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+API_KEY = os.getenv("HONEYPOT_API_KEY", "SECRET_KEY")
